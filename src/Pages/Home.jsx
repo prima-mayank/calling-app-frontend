92a807938cdeb1da3c1789b99ab343966c2d3929
Mayank Shakya <mayank.shakya@primathon.in>
2026-01-31 22:39:24 +0530

    room page added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import CreateRoom from "../components/CreateRoom";
+
+const Home = () => {
+  return (
+    <div className="h-[100vh] flex flex-col gap-4 items-center justify-center">
+      <CreateRoom />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/CreateRoom.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { SocketContext } from "../Context/SocketContext";
+
+const CreateRoom = () => {
+  const { socket } = useContext(SocketContext);
+
+  const initRoom = () => {
+    // console.log("Initialising a req to create a room", socket);
+    socket.emit("create-room");
+  };
+
+  return (
+    <button
+      onClick={initRoom}
+      className="btn btn-secondary"
+    >
+      Start a new meeting in a new room
+    </button>
+  );
+};
+
+export default CreateRoom;
