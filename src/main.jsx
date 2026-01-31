45fd4bbaaa3d5599b1c43d2e1371ff7b59b54be8
Mayank Shakya <mayank.shakya@primathon.in>
2026-01-31 22:25:14 +0530

    add basic files

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { SocketProvider } from "./Context/SocketContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <SocketProvider>
+      <App />
+    </SocketProvider>
+  </BrowserRouter>
+);
